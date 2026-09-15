15f11e5aa87e3b3f4e4dc1aded408254b180e8dd
Stepan Tarabin <stepantarabin16@gmail.com>
2026-09-16 00:07:32 +0300

    feat: README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# FLangInterpreter
+> by LaPolicia
+
+# How to launch
+You can use convenient `run.sh` script:
+```shell
+./run.sh  # prompt mode
+./run.sh sc-examples/1.txt
+```
+
+# Code Formatting
+***PLEASE*** use Spotless before commiting
+```shell
+./gradlew spotlessCheck  # show inconsistencies
+./gradlew spotlessApply  # fix them
+```
--- app/src/main/java/ru/lapolitia/Flang.java
+++ app/src/main/java/ru/lapolitia/Flang.java
@@ -26,7 +26,8 @@ public class Flang {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
-        if (hasError) System.exit(2);
+        if (hasError)
+            System.exit(2);
     }
 
     private static void runPrompt() throws IOException {
@@ -36,7 +37,8 @@ public class Flang {
         while (true) {
             System.out.print("> ");
             String line = reader.readLine();
-            if (line == null) break;
+            if (line == null)
+                break;
             run(line);
             // to prevent killing the user session
             hasError = false;
